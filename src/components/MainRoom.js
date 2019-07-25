c53cd45a49c476d037eccd66efb135368bda357d
Jose Octavio de Castro Neves Jr <joc@fullsquad.com.br>
2019-07-25 09:17:27 -0300

    rewinding to v0.1

--- src/components/MainRoom.js
+++ src/components/MainRoom.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Lounge from './Lounge';
 
-const MainRoom = ({ orders, addOrder, loungeOrders }) => {
+const MainRoom = ({ orders, addOrder }) => {
   return (
     <div className="component">
       <h1>MainRoom</h1>
       <h2>Orders: {orders}</h2>
       <h3>props.orders</h3>
       <h3>props.addOrder</h3>
-      <button onClick={() => addOrder('mainRoomOrders')}>Add new order</button>
-      <Lounge totalOrders={loungeOrders} addNewOrder={addOrder} />
+      <button onClick={() => addOrder()}>Add new order</button>
+      <Lounge totalOrders={orders} addNewOrder={addOrder} />
     </div>
   )
 }
